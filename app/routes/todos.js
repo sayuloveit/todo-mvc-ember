e915eebf3cc6f84d35407a5419ed85f9115dd983
vic <vicxizhu@gmail.com>
2015-08-23 21:16:27 -0400

    add edit and delete

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -3,5 +3,28 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return this.store.find('todo');
+  },
+  actions: {
+    createTodo: function(newToDo) {
+
+        var todo = this.store.createRecord('todo', {
+            title: newToDo,
+            isCompleted: false
+        });
+
+        this.controllerFor('todos').set('newToDo', '');
+
+        todo.save();
+    },
+    acceptChanges: function(todo) {
+      if (Ember.isEmpty(todo.get('title'))) {
+          this.send('deleteTodo', todo);
+      } else {
+          todo.save();
+      }
+    },
+    deleteTodo: function(todo) {
+        todo.deleteRecord();
+    }
   }
 });
