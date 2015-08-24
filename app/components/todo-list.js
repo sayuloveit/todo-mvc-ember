9c7b2fc5dee3eb36c3e21d6be196bd7eb118179e
vic <vicxizhu@gmail.com>
2015-08-24 15:34:58 -0400

    add clearCompleted action, completed and hasCompleted properties

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -5,6 +5,11 @@ export default Ember.Component.extend({
     createTodo: function(newTitle) {
       this.set('newTitle', '');
       this.sendAction('createTodo', newTitle);
+    },
+    clearCompleted: function() {
+      var completed =this.get('model').filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
   remaining: Ember.computed('model.@each.isCompleted', function() {
@@ -14,5 +19,12 @@ export default Ember.Component.extend({
   inflection: Ember.computed('remaining', function() {
     var remaining = this.get('remaining');
     return (remaining === 1) ? 'item' : 'items';
+  }),
+  completed: Ember.computed('model.@each.isCompleted', function(){
+    var model = this.get('model');
+    return model.filterBy('isCompleted', true).get('length');
+  }),
+  hasCompleted: Ember.computed('completed', function(){
+    return this.get('completed') > 0;
   })
 });
