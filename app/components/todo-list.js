f99684d9eeaecea1e8fd723903caf1659f1c4064
vic <vicxizhu@gmail.com>
2015-08-24 15:09:57 -0400

    add create action, inflection and remaining properties

---
+++ app/components/todo-list.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    createTodo: function(newTitle) {
+      this.set('newTitle', '');
+      this.sendAction('createTodo', newTitle);
+    }
+  },
+  remaining: Ember.computed('model.@each.isCompleted', function() {
+    var model = this.get('model');
+    return model.filterBy('isCompleted', false).get('length');
+  }),
+  inflection: Ember.computed('remaining', function() {
+    var remaining = this.get('remaining');
+    return (remaining === 1) ? 'item' : 'items';
+  })
+});
